e983ed4b5607efeb74bf7235079492b0e441b498
gaetan <gaetan.depin@fr.clara.net>
2019-10-04 15:16:31 +0200

    update template resource

--- provider/resource_zabbix_template.go
+++ provider/resource_zabbix_template.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
@@ -41,15 +42,70 @@ func resourceZabbixTemplate() *schema.Resource {
 				Optional:    true,
 				Description: "Description of the template",
 			},
+			"macro": &schema.Schema{
+				Type:     schema.TypeMap,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+			"linked_template": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+			"linked_host": &schema.Schema{
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
 		},
 	}
 }
 
+func createZabbixMacro(d *schema.ResourceData) zabbix.Macros {
+	var macros zabbix.Macros
+
+	terraformMacros := d.Get("macro").(map[string]interface{})
+	for i, terraformMacro := range terraformMacros {
+		macro := zabbix.Macro{
+			MacroName: fmt.Sprintf("{$%s}", i),
+			Value:     terraformMacro.(string),
+		}
+		macros = append(macros, macro)
+	}
+	return macros
+}
+
+func createLinkedTemplate(d *schema.ResourceData) zabbix.Templates {
+	var templates zabbix.Templates
+
+	terraformTemplates := d.Get("linked_template").(*schema.Set)
+	for _, terraformTemplate := range terraformTemplates.List() {
+		zabbixTemplate := zabbix.Template{
+			TemplateID: terraformTemplate.(string),
+		}
+		templates = append(templates, zabbixTemplate)
+	}
+	return templates
+}
+
+func createLinkedHost(d *schema.ResourceData) []string {
+	var hosts []string
+
+	terraformHosts := d.Get("linked_host").(*schema.Set)
+	for _, terraformHost := range terraformHosts.List() {
+		hosts = append(hosts, terraformHost.(string))
+	}
+	return hosts
+}
+
 func createTemplateObj(d *schema.ResourceData, api *zabbix.API) (*zabbix.Template, error) {
 	template := zabbix.Template{
-		Host:        d.Get("host").(string),
-		Name:        d.Get("name").(string),
-		Description: d.Get("description").(string),
+		Host:            d.Get("host").(string),
+		Name:            d.Get("name").(string),
+		Description:     d.Get("description").(string),
+		UserMacros:      createZabbixMacro(d),
+		LinkedTemplates: createLinkedTemplate(d),
+		LinkedHosts:     createLinkedHost(d),
 	}
 	hostGroupIDs, err := getHostGroups(d, api)
 	if err != nil {
@@ -84,32 +140,36 @@ func resourceZabbixTemplateCreate(d *schema.ResourceData, meta interface{}) erro
 func resourceZabbixTemplateRead(d *schema.ResourceData, meta interface{}) error {
 	api := meta.(*zabbix.API)
 
-	template, err := api.TemplateGetByID(d.Id())
+	params := zabbix.Params{
+		"templateids":     d.Id(),
+		"selectHosts":     "extend",
+		"selectTemplates": "extend",
+		"output":          "extend",
+		"selectMacros":    "extend",
+	}
+	templates, err := api.TemplatesGet(params)
 	if err != nil {
 		return err
 	}
 
+	template := templates[0]
 	d.Set("host", template.Host)
-	d.Set("name", template.Name)
+	if template.Host != template.Name && d.Get("name").(string) == "" {
+		d.Set("name", template.Name)
+	}
 	d.Set("description", template.Description)
 
-	params := zabbix.Params{
-		"output": "extend",
-		"hostids": []string{
-			d.Id(),
-		},
-	}
-
-	groups, err := api.HostGroupsGet(params)
+	terraformMacros, err := createTerraformMacro(template)
 	if err != nil {
 		return err
 	}
+	d.Set("macro", terraformMacros)
 
-	groupNames := make([]string, len(groups))
-	for i, g := range groups {
-		groupNames[i] = g.Name
+	terraformGroups, err := createTerraformTemplateGroup(d, api)
+	if err != nil {
+		return err
 	}
-	d.Set("groups", groupNames)
+	d.Set("groups", terraformGroups)
 	return nil
 }
 
@@ -149,4 +209,63 @@ func resourceZabbixTemplateDelete(d *schema.ResourceData, meta interface{}) erro
 	api := meta.(*zabbix.API)
 
 	return api.TemplatesDeleteByIds([]string{d.Id()})
+}
+
+func createTerraformMacro(template zabbix.Template) (map[string]interface{}, error) {
+	terraformMacros := make(map[string]interface{}, len(template.UserMacros))
+	log.Printf("size %d", len(template.UserMacros))
+	for _, macro := range template.UserMacros {
+		var name string
+		if noPrefix := strings.Split(macro.MacroName, "{$"); len(noPrefix) == 2 {
+			name = noPrefix[1]
+		} else {
+			return nil, fmt.Errorf("Invalid macro name \"%s\"", macro.MacroName)
+		}
+		if noSuffix := strings.Split(name, "}"); len(noSuffix) == 2 {
+			name = noSuffix[0]
+		} else {
+			return nil, fmt.Errorf("ll Invalid macro name \"%s\"", macro.MacroName)
+
+		}
+		terraformMacros[name] = macro.Value
+	}
+	return terraformMacros, nil
+}
+
+func createTerraformTemplateGroup(d *schema.ResourceData, api *zabbix.API) ([]string, error) {
+	params := zabbix.Params{
+		"output": "extend",
+		"hostids": []string{
+			d.Id(),
+		},
+	}
+
+	groups, err := api.HostGroupsGet(params)
+	if err != nil {
+		return nil, err
+	}
+
+	groupNames := make([]string, len(groups))
+	for i, g := range groups {
+		groupNames[i] = g.Name
+	}
+	return groupNames, nil
+}
+
+func createTerraformLinkedTemplate(template zabbix.Template) []string {
+	var terraformTemplates []string
+
+	for _, linkedTemplate := range template.LinkedTemplates {
+		terraformTemplates = append(terraformTemplates, linkedTemplate.TemplateID)
+	}
+	return terraformTemplates
+}
+
+func createTerraformLinkedHost(template zabbix.Template) []string {
+	var terraformHosts []string
+
+	for _, linkedHost := range template.LinkedHosts {
+		terraformHosts = append(terraformHosts, linkedHost)
+	}
+	return terraformHosts
 }
